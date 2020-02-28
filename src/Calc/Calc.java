ae1d4804f1acc3e5188762362501e0a8c51af023
Sewar <Sewar@DESKTOP-QQU258B>
2020-02-28 15:37:59 +0200

    calculater with test cases

---
+++ src/Calc/Calc.java
@@ -0,0 +1,16 @@
+package Calc;
+
+public class Calc {
+private int x;
+private int y;
+public int add(int xx,int yy) {
+	x=xx;
+	y=yy;
+	return x+y;
+}
+public int sub(int xx,int yy) {
+	x=xx;
+	y=yy;
+	return x-y;
+}
+}
---
+++ src/CalcTest/CalcTst.java
@@ -0,0 +1,90 @@
+package CalcTest; 
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+import Calc.Calc;
+
+public class CalcTst {
+
+	@Test
+	public void testAdd_TwoPosInt_PosInt() {
+		
+		Calc o=new Calc();
+		int res=o.add(5,8);
+		assertTrue(res==13);
+	}
+	@Test
+	public void testAdd_TwoNegInt_NegInt() {
+		
+		Calc o=new Calc();
+		int res=o.add(-5,-8);
+		assertTrue(res==-13);
+	}
+	
+	@Test
+	public void testAdd_OnePosIntWith0_PosInt() {
+		
+		Calc o=new Calc();
+		int res=o.add(5,0);
+		assertTrue(res==5);
+	}
+	@Test
+	public void testAdd_OneNegIntWith0_NegInt() {
+		
+		Calc o=new Calc();
+		int res=o.add(-5,0);
+		assertTrue(res==-5);
+	}
+	@Test (timeout =1)
+	public void testAdd_FirstPosIntSecondNeg_GreaterInt() {
+		
+		Calc o=new Calc();
+		int res=o.add(5,-8);
+		assertTrue(res==-3);
+	}
+	@Test
+	public void testAdd_FirstNegIntSecondNeg_GreaterInt() {
+		
+		Calc o=new Calc();
+		int res=o.add(-5,8);
+		assertTrue(res==3);
+	}
+	@Test
+	public void testAdd_LitterWithDigit_exception() throws Exception{
+		
+		Calc o=new Calc();
+		int res=o.add(5,'w');
+		assertTrue(res==0);
+	}
+	@Test
+	public void testSub_FirstPosGreaterSecondPosLess_PostInt() {
+		
+		Calc o=new Calc();
+		int res=o.sub(8,5);
+		assertTrue(res==3);
+	}
+	@Test
+	public void testSub_FirstLessIntSecondGreater_NegInt() {
+		
+		Calc o=new Calc();
+		int res=o.sub(5,8);
+		assertTrue(res==-3);
+	}
+	@Test
+	public void testSub_FirstNegIntSecondPos_NegInt() {
+		
+		Calc o=new Calc();
+		int res=o.sub(-5,8);
+		assertTrue(res==-13);
+	}
+	@Test
+	public void testSub_FirstPosIntSecondNeg_PostInt() {
+		
+		Calc o=new Calc();
+		int res=o.sub(5,-8);
+		assertTrue(res==13);
+	}
+	
+}
